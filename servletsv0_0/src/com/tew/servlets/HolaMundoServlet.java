f3ffdfc8112efc83e10c92a59b478e76930c7fef
miermontoto <UO283319@uniovi.es>
2023-09-18 13:22:46 +0200

    TEW-2324-4-UO283319-2.4-Encadenamiento de Servlets

--- servletsv0_0/src/com/tew/servlets/HolaMundoServlet.java
+++ servletsv0_0/src/com/tew/servlets/HolaMundoServlet.java
@@ -3,6 +3,8 @@ package com.tew.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Vector;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,45 +17,24 @@ public class HolaMundoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String nombre = (String) request.getParameter("NombreUsuario");
 		HttpSession session = request.getSession();
 		
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
+		String nombre = (String) request.getParameter("NombreUsuario");
 		Vector listado = (Vector) session.getAttribute("listado");
+		Integer contador = (Integer) getServletContext().getAttribute("contador");
+		
 		if (listado == null) listado = new Vector();
 		
-		Integer contador = (Integer) getServletContext().getAttribute("contador");
 		if (contador == null) contador = new Integer(0);
 		getServletContext().setAttribute("contador", new Integer(contador.intValue() + 1));
 		
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-		out.println("<BODY>");
-		
 		if (nombre != null) {
-			out.println("<br>Hola " + nombre + ",<br>");
 			listado.addElement(nombre);
 		}
 		session.setAttribute("listado", listado);
 		
-		out.println("Bienvenido a mi primera página web!");
-		
-		if(listado.size() == 0) {
-			out.println("<br><br>Eres la primera persona en visitar esta página!");
-		} else {
-			out.println("<br><br>Contigo, hoy me han visitado:");
-			for(int i = 0; i < listado.size(); i++) {
-				out.println("<br> - " + listado.get(i).toString());
-			}
-		}
-		
-		
-		out.println("<br><br>Visitas: " + contador);
-		out.println("<br><a href=\"index.html\">Volver</a>");
-		out.println("</BODY></HTML>");
+		RequestDispatcher dispatcher = getServletContext().getNamedDispatcher("HolaMundoVista");
+		dispatcher.forward(request, response);
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
---
+++ servletsv0_0/src/com/tew/servlets/HolaMundoVistaServlet.java
@@ -0,0 +1,52 @@
+package com.tew.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HolaMundoVista", urlPatterns = { "/HolaMundoVista" })
+public class HolaMundoVistaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = (String) request.getParameter("NombreUsuario");
+		Vector listado = (Vector) request.getSession().getAttribute("listado");
+		Integer contador = (Integer) getServletContext().getAttribute("contador");
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<HTML>");
+		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+		out.println("<BODY>");
+		
+		out.println("<br>Hola " + nombre + ",<br>");
+		out.println("Bienvenido a mi primera página web!");
+
+		if (listado.size() == 0) {
+			out.println("<br><br>Eres la primera persona en visitar esta página!");
+		} else {
+			out.println("<br><br>Contigo, hoy me han visitado:");
+			for (int i = 0; i < listado.size(); i++) {
+				out.println("<br> - " + listado.get(i).toString());
+			}
+		}
+		
+		
+		out.println("<br><br>Visitas: " + contador);
+		out.println("<br><a href=\"index.html\">Volver</a>");
+		out.println("</BODY></HTML>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
