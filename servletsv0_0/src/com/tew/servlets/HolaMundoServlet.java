313e2be8f68eac8f4a64c7bb941d0ec7dd9f8771
miermontoto <UO283319@uniovi.es>
2023-09-18 12:29:42 +0200

    TEW-2324-4-UO283319-2.2-Servlets con sesiones

--- servletsv0_0/src/com/tew/servlets/HolaMundoServlet.java
+++ servletsv0_0/src/com/tew/servlets/HolaMundoServlet.java
@@ -2,11 +2,13 @@ package com.tew.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "HolaMundo", urlPatterns = { "/HolaMundoCordial" })
 public class HolaMundoServlet extends HttpServlet {
@@ -14,16 +16,32 @@ public class HolaMundoServlet extends HttpServlet {
 
 	public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String nombre = (String) request.getParameter("NombreUsuario");
+		HttpSession session = request.getSession();
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		
+		Vector listado = (Vector) session.getAttribute("listado");
+		if (listado == null) listado = new Vector();
+		
 		out.println("<HTML>");
 		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
 		out.println("<BODY>");
 		
-		if (nombre != null) out.println("<br>Hola " + nombre + ",<br>");
+		if (nombre != null) {
+			out.println("<br>Hola " + nombre + ",<br>");
+			listado.addElement(nombre);
+		}
+		session.setAttribute("listado", listado);
 		
 		out.println("Bienvenido a mi primera página web!");
+		out.println("<br><br>Contigo, hoy me han visitado:");
+		for(int i = 0; i < listado.size(); i++) {
+			out.println("<br>" + listado.get(i).toString());
+		}
+		
+		out.println("<br><br><a href=\"index.html\">Volver</a>");
 		out.println("</BODY></HTML>");
 	}
 
