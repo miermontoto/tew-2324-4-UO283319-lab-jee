511843e8276ace238677ec8f7ff3d3dd5a536c00
miermontoto <UO283319@uniovi.es>
2023-10-01 14:16:48 +0200

    TEW-2324-4-UO283319-3.1-Managed Bean BeanAlumnos basico

---
+++ gestioneitorv1_0/src/com/tew/presentation/BeanAlumnos.java
@@ -0,0 +1,32 @@
+package com.tew.presentation;
+import java.io.Serializable;
+import javax.faces.bean.*;
+import javax.faces.event.ActionEvent;
+import com.tew.business.AlumnosService;
+import com.tew.infrastructure.Factories;
+import com.tew.model.Alumno;
+
+@ManagedBean
+@SessionScoped
+public class BeanAlumnos implements Serializable {
+	private static final long serialVersionUID = 55555L;
+
+	// Se añade este atributo de entidad para recibir el alumno concreto seleccionado
+	// de la tabla o de un formulario.
+	// Es necesario inicializarlo para que al entrar desde el formulario de
+	// AltaForm.xhtml se puedan dejar los valores en un objeto existente.
+	private Alumno alumno = new Alumno();
+	private Alumno[] alumnos = null;
+
+	public BeanAlumnos() {
+		iniciaAlumno(null);
+	}
+
+	public void iniciaAlumno(ActionEvent event) {
+		alumno.setId(null);
+		alumno.setIduser("IdUser");
+		alumno.setNombre("Nombre");
+		alumno.setApellidos("Apellidos");
+		alumno.setEmail("email@domain.com");
+	}
+}
