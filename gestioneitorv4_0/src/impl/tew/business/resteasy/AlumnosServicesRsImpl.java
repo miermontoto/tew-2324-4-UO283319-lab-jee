dc20150dfc53b1ead008e2d91c5874f31a536b70
miermontoto <UO283319@uniovi.es>
2023-11-03 19:49:34 +0100

    TEW-2324-4-UO283319-7.8-Ejercicios complementario Alta de Alumno

--- gestioneitorv4_0/src/impl/tew/business/resteasy/AlumnosServicesRsImpl.java
+++ gestioneitorv4_0/src/impl/tew/business/resteasy/AlumnosServicesRsImpl.java
@@ -1,6 +1,11 @@
 package impl.tew.business.resteasy;
 
 import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+
 import com.tew.business.resteasy.AlumnosServicesRs;
 import com.tew.business.exception.EntityAlreadyExistsException;
 import com.tew.business.exception.EntityNotFoundException;
@@ -14,6 +19,8 @@ public class AlumnosServicesRsImpl implements AlumnosServicesRs {
 	}
 
 	@Override
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public void saveAlumno(Alumno alumno) throws EntityAlreadyExistsException {
 		new AlumnosAlta().save(alumno);
 	}
